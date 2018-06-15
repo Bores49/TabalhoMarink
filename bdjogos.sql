47a64c021de5381422d074582d28acc42f65dea9
Bores49 <40175900+Bores49@users.noreply.github.com>
2018-06-15 12:08:01 -0300

    Add files via upload

--- bdjogos.sql
+++ bdjogos.sql
@@ -16,30 +16,60 @@
 CREATE DATABASE IF NOT EXISTS `bdjogos` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `bdjogos`;
 
+-- Copiando estrutura para tabela bdjogos.fabricante
+CREATE TABLE IF NOT EXISTS `fabricante` (
+  `codigoFabri` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`codigoFabri`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela bdjogos.fabricante: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `fabricante` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fabricante` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela bdjogos.games
 CREATE TABLE IF NOT EXISTS `games` (
   `codigo` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) DEFAULT NULL,
-  `genero` varchar(20) DEFAULT NULL,
-  `plataforma` varchar(20) DEFAULT NULL,
   `tamanho` double DEFAULT NULL,
-  `fabricante` varchar(50) NOT NULL,
   PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
--- Copiando dados para a tabela bdjogos.games: ~8 rows (aproximadamente)
+-- Copiando dados para a tabela bdjogos.games: ~9 rows (aproximadamente)
 /*!40000 ALTER TABLE `games` DISABLE KEYS */;
-INSERT INTO `games` (`codigo`, `nome`, `genero`, `plataforma`, `tamanho`, `fabricante`) VALUES
-	(1, NULL, 'Ação', 'PC', 50.400001525878906, 'Rockstar'),
-	(2, NULL, 'RPG', 'PS4', 23.399999618530273, 'sdg'),
-	(3, 'sdfg', 'MMORPG', 'PS4', 56, 'pau'),
-	(4, 'sdfg', 'Ação', 'PC', 23, 'fsdag'),
-	(5, 'sdg', 'Corrida', 'PC', 4, 'sdg'),
-	(6, 'sdg', 'Corrida', 'PS4', 4, 'sdg'),
-	(7, 'hsfdhg', 'MOBA', 'PC', 34, 'dhn'),
-	(8, 'oluugjl', 'Aventura', 'Xbox One', 34, 'lh'),
-	(9, 'egffdshg', 'Ação', 'Xbox One', 45, 'asfsdg');
+INSERT INTO `games` (`codigo`, `nome`, `tamanho`) VALUES
+	(1, NULL, NULL),
+	(2, NULL, NULL),
+	(3, 'sdfg', NULL),
+	(4, 'sdfg', NULL),
+	(5, 'sdg', NULL),
+	(6, 'sdg', NULL),
+	(7, 'hsfdhg', NULL),
+	(8, 'oluugjl', NULL),
+	(9, 'egffdshg', NULL);
 /*!40000 ALTER TABLE `games` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela bdjogos.genero
+CREATE TABLE IF NOT EXISTS `genero` (
+  `codigoGene` int(11) NOT NULL AUTO_INCREMENT,
+  `genero` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`codigoGene`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela bdjogos.genero: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `genero` DISABLE KEYS */;
+/*!40000 ALTER TABLE `genero` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela bdjogos.plataforma
+CREATE TABLE IF NOT EXISTS `plataforma` (
+  `codigoPlat` int(11) NOT NULL AUTO_INCREMENT,
+  `plataforma` varchar(50) DEFAULT '0',
+  PRIMARY KEY (`codigoPlat`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela bdjogos.plataforma: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `plataforma` DISABLE KEYS */;
+/*!40000 ALTER TABLE `plataforma` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
